dfc0edd3b9ab1c2c997d41669b31afbd1994e5f9
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2022-02-17 16:35:45 +0100

    Make allocation functions inline

--- src/fu/core/alloc.h
+++ src/fu/core/alloc.h
@@ -1,10 +1,34 @@
 #ifndef FU_CORE_ALLOC_H
 #define FU_CORE_ALLOC_H
 
+#include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
-void* malloc_or_die(size_t);
-void* calloc_or_die(size_t, size_t);
-void* realloc_or_die(void*, size_t);
+static inline void die(const char* msg) {
+    fputs(msg, stderr);
+    abort();
+}
+
+static inline void* malloc_or_die(size_t size) {
+    void* ptr = malloc(size);
+    if (!ptr)
+        die("out of memory, malloc() failed\n");
+    return ptr;
+}
+
+static inline void* calloc_or_die(size_t count, size_t size) {
+    void* ptr = calloc(count, size);
+    if (!ptr)
+        die("out of memory, calloc() failed\n");
+    return ptr;
+}
+
+static inline void* realloc_or_die(void* ptr, size_t size) {
+    ptr = realloc(ptr, size);
+    if (!ptr)
+        die("out of memory, realloc() failed\n");
+    return ptr;
+}
 
 #endif
